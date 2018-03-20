45fea0f4a8dead993974331526bd21ca5bf770c5
ADHIBI TAIB <rev.damage@gmail.com>
2018-03-20 17:48:53 +0100

    ADDED package metier(Services) ADDED JSP ( jars + config )

---
+++ src/java/com/crm1/metier/ClientServices.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crm1.metier;
+
+import com.crm1.beans.Client;
+import java.util.List;
+
+/**
+ *
+ * @author REV DAMAGE
+ */
+public interface ClientServices {
+    
+    public void add(Client c);
+    public Client edit(Client c);
+    public Client delete(Integer idCli);
+    public List<Client> findAll();
+    public Client findByidCli(Integer idCli);
+    
+}
---
+++ src/java/com/crm1/metier/ClientServicesImpl.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crm1.metier;
+
+import com.crm1.beans.Client;
+import com.crm1.dao.ClientDAO;
+import com.crm1.dao.ClientDAOImpl;
+import java.util.List;
+
+/**
+ *
+ * @author REV DAMAGE
+ */
+public class ClientServicesImpl implements ClientServices {
+    
+    ClientDAO dao = new ClientDAOImpl();
+
+    @Override
+    public void add(Client c) {
+        dao.add(c); 
+    }
+
+    @Override
+    public Client edit(Client c) {
+        return dao.edit(c); 
+    }
+
+    @Override
+    public Client delete(Integer idCli) {
+        dao.delete(idCli); 
+        return null;
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return dao.findAll(); 
+    }
+
+    @Override
+    public Client findByidCli(Integer idCli) {
+        return dao.findByidCli(idCli); 
+    }
+    
+}
